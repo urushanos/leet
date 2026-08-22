b921a41955601f287a372dd475938b1d0d881de2
urushanos <urvimapsenkar@gmail.com>
2026-08-22 19:31:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (91.43%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        if(s=="") return true;
-        if(s.size() > t.size()) return false;
-
         int slow=0, fast=0;
 
         while(fast < t.size() && slow<s.size()){
             if(s[slow] == t[fast]) {
-                //if(s[s.size()-1] == t[fast]) return true;    
                 slow++;
             }
             fast++;
         }
 
-        if(slow == s.size()) return true;
-        else return false;
+        return slow==s.size();
     }
 };
